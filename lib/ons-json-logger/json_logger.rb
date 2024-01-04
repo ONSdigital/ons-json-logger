7eb881a7b392f024b41ee93bed0f9089e5301f40
John Topley <john.topley@ons.gov.uk>
2024-01-04 13:12:18 +0000

    Corrected documentation comments

--- lib/ons-json-logger/json_logger.rb
+++ lib/ons-json-logger/json_logger.rb
@@ -8,7 +8,7 @@ class JSONLogger
   # Constructor that initialises the JSON logger.
   #
   # @param application [String] the name of the application
-  # @param environment [String] the name of the environment the application is deployed to. Defaults to +development+
+  # @param environment [String] the name of the environment the application is deployed to
   # @raise [ArgumentError] if application is nil
   def initialize(application:, environment: 'development')
     raise ArgumentError, 'application cannot be nil' if application.nil?
@@ -22,11 +22,11 @@ class JSONLogger
   # @param level the log level e.g. +DEBUG+, +ERROR+, +INFO+
   # @param message the log message
   # @param module_name the name of the application module/component generating the log message
-  # user optional Hash containing details of the user making the request such as username and IP address
-  # http optional Hash containing details of the HTTP request such as the method, path, status code and user agent
-  # error optional Hash containing details of an error that occurred such as the error code and message
+  # @param user optional Hash containing details of the user making the request such as username and IP address
+  # @param http optional Hash containing details of the HTTP request such as the method, path, status code and user agent
+  # @param error optional Hash containing details of an error that occurred such as the error code and message
   # @raise [ArgumentError] if level is nil
-  # @raise [ArgumentError] id message is nil
+  # @raise [ArgumentError] if message is nil
   # @raise [ArgumentError] if module_name is nil
   def log(level:, message:, module_name:, user: {}, http: {}, error: {})
     raise ArgumentError, 'level cannot be nil' if level.nil?
